5ca8b588d9b9bcea46d54ab2de6dbf377c407065
Evan Hazlett <ejhazlett@gmail.com>
2014-08-14 11:12:36 -0400

    better error handling for events

--- controller/handler.go
+++ controller/handler.go
@@ -16,18 +16,18 @@ func (l *EventHandler) Handle(e *citadel.Event) error {
 	logger.Infof("event: date=%s type=%s image=%s container=%s", e.Time.Format(time.RubyDate), e.Type, e.Container.Image.Name, e.Container.ID[:12])
 	switch e.Type {
 	case "start", "restart":
-		l.handleUpdate(e)
+		return l.handleUpdate(e)
 	case "stop", "kill", "die":
 		// add delay to make sure container is removed
 		time.Sleep(250 * time.Millisecond)
-		l.handleUpdate(e)
+		return l.handleUpdate(e)
 	}
 	return nil
 }
 
 func (l *EventHandler) handleUpdate(e *citadel.Event) error {
 	if err := l.Manager.UpdateConfig(e); err != nil {
-		logger.Fatal(err)
+		return err
 	}
 	if err := l.Manager.Reload(); err != nil {
 		return err
