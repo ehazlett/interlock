89c4319ba21b6274cfcf4e6cee41c4ee88a0df84
Evan Hazlett <ejhazlett@gmail.com>
2014-09-11 19:51:46 -0400

    shipyard support

--- config.go
+++ config.go
@@ -13,16 +13,18 @@ type (
 	}
 
 	Config struct {
-		ProxyConfigPath  string             `json:"proxy_config_path,omitempty"`
-		Port             int                `json:"port,omitempty"`
-		PidPath          string             `json:"pid_path,omitempty"`
-		ConnectTimeout   int                `json:"connect_timeout,omitempty"`
-		ServerTimeout    int                `json:"server_timeout,omitempty"`
-		ClientTimeout    int                `json:"client_timeout,omitempty"`
-		MaxConn          int                `json:"max_conn,omitempty"`
-		SyslogAddr       string             `json:"syslog_addr,omitempty"`
-		StatsUser        string             `json:"stats_user,omitempty"`
-		StatsPassword    string             `json:"stats_password,omitempty"`
-		InterlockEngines []*InterlockEngine `json:"engines,omitempty"`
+		ProxyConfigPath    string             `json:"proxy_config_path,omitempty"`
+		Port               int                `json:"port,omitempty"`
+		PidPath            string             `json:"pid_path,omitempty"`
+		ConnectTimeout     int                `json:"connect_timeout,omitempty"`
+		ServerTimeout      int                `json:"server_timeout,omitempty"`
+		ClientTimeout      int                `json:"client_timeout,omitempty"`
+		MaxConn            int                `json:"max_conn,omitempty"`
+		SyslogAddr         string             `json:"syslog_addr,omitempty"`
+		StatsUser          string             `json:"stats_user,omitempty"`
+		StatsPassword      string             `json:"stats_password,omitempty"`
+		InterlockEngines   []*InterlockEngine `json:"engines,omitempty"`
+		ShipyardUrl        string             `json:"shipyard_url,omitempty"`
+		ShipyardServiceKey string             `json:"shipyard_service_key,omitempty"`
 	}
 )
