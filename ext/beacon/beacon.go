4a05af35ed78acd816ac2e0f2008a0b4f0071a3d
Evan Hazlett <ejhazlett@gmail.com>
2016-03-03 16:29:59 -0500

    beacon extension
    Signed-off-by: Evan Hazlett <ejhazlett@gmail.com>

--- ext/beacon/beacon.go
+++ ext/beacon/beacon.go
@@ -37,9 +37,8 @@ func NewBeacon(c *config.ExtensionConfig, client *dockerclient.DockerClient) (*B
 
 	errChan = make(chan error)
 	go func() {
-		for err := range errChan {
-			log().Error(err)
-		}
+		err := <-errChan
+		log().Error(err)
 	}()
 
 	ext := &Beacon{
@@ -50,36 +49,67 @@ func NewBeacon(c *config.ExtensionConfig, client *dockerclient.DockerClient) (*B
 	return ext, nil
 }
 
-func (b *Beacon) Name() string {
-	return pluginName
+func (b *Beacon) Update() error {
+	// update is not handled by beacon
+	return nil
+}
+
+func (b *Beacon) Reload() error {
+	// reload is not handled by beacon
+	return nil
 }
 
 func (b *Beacon) HandleEvent(event *dockerclient.Event) error {
 	switch event.Status {
-	case "interlock-start":
-		// scan all containers and start metrics
-		containers, err := b.client.ListContainers(false, false, "")
+	case "start":
+		// get container info for event
+		c, err := b.client.InspectContainer(event.ID)
 		if err != nil {
 			return err
 		}
 
-		for _, c := range containers {
-			if err := b.startStats(c.Id); err != nil {
-				log().Warnf("unable to start stats for containers: id=%s err=%s", c.Id, err)
-				continue
-			}
+		image := c.Config.Image
+
+		// TODO: match rules
+		if !b.ruleMatch(c.Config) {
+			return nil
 		}
-	case "start":
-		log().Debugf("checking container for collection: id=%s", event.ID)
-		if err := b.startStats(event.ID); err != nil {
+
+		log().Debugf("starting collection: id=%s image=%s", event.ID, image)
+		if err := b.startStats(event.ID, image); err != nil {
 			return err
 		}
 	case "kill", "die", "stop", "destroy":
-		log().Debugf("resetting stats: id=%s", event.ID)
-		if err := b.resetStats(event.ID); err != nil {
+		c, err := b.client.InspectContainer(event.ID)
+		if err != nil {
+			return err
+		}
+
+		image := c.Config.Image
+		log().Debugf("resetting stats: id=%s image=%s", event.ID, image)
+		if err := b.resetStats(event.ID, image); err != nil {
 			return err
 		}
 	}
 
 	return nil
+}
+
+func (b *Beacon) ruleMatch(cfg *dockerclient.ContainerConfig) bool {
+	// TODO
+	return true
+}
+
+func (b *Beacon) startStats(id string, image string) error {
+	log().Debugf("gathering stats: id=%s image=%s interval=%d", id, image, b.cfg.StatInterval)
+	args := eventArgs{
+		Image: image,
+	}
+	go b.handleStats(id, b.sendContainerStats, errChan, args)
+
+	return nil
+}
+
+func (b *Beacon) handleStats(id string, cb dockerclient.StatCallback, ec chan error, args ...interface{}) {
+	go b.client.StartMonitorStats(id, cb, ec, args...)
 }
