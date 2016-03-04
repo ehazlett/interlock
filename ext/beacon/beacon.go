c0efd90ee899a73905cdcb5e82457d58c4b8f8df
Evan Hazlett <ejhazlett@gmail.com>
2016-03-03 23:20:09 -0500

    update error handling and extension loading
    Signed-off-by: Evan Hazlett <ejhazlett@gmail.com>

--- ext/beacon/beacon.go
+++ ext/beacon/beacon.go
@@ -37,14 +37,9 @@ func NewBeacon(c *config.ExtensionConfig, client *dockerclient.DockerClient) (*B
 
 	errChan = make(chan error)
 	go func() {
-<<<<<<< HEAD
 		for err := range errChan {
 			log().Error(err)
 		}
-=======
-		err := <-errChan
-		log().Error(err)
->>>>>>> af48d45... beacon extension
 	}()
 
 	ext := &Beacon{
@@ -55,10 +50,6 @@ func NewBeacon(c *config.ExtensionConfig, client *dockerclient.DockerClient) (*B
 	return ext, nil
 }
 
-<<<<<<< HEAD
-func (b *Beacon) Name() string {
-	return pluginName
-=======
 func (b *Beacon) Update() error {
 	// update is not handled by beacon
 	return nil
@@ -67,40 +58,17 @@ func (b *Beacon) Update() error {
 func (b *Beacon) Reload() error {
 	// reload is not handled by beacon
 	return nil
->>>>>>> af48d45... beacon extension
 }
 
 func (b *Beacon) HandleEvent(event *dockerclient.Event) error {
 	switch event.Status {
-<<<<<<< HEAD
-	case "interlock-start":
-		// scan all containers and start metrics
-		containers, err := b.client.ListContainers(false, false, "")
-=======
 	case "start":
 		// get container info for event
 		c, err := b.client.InspectContainer(event.ID)
->>>>>>> af48d45... beacon extension
 		if err != nil {
 			return err
 		}
 
-<<<<<<< HEAD
-		for _, c := range containers {
-			if err := b.startStats(c.Id); err != nil {
-				log().Warnf("unable to start stats for containers: id=%s err=%s", c.Id, err)
-				continue
-			}
-		}
-	case "start":
-		log().Debugf("checking container for collection: id=%s", event.ID)
-		if err := b.startStats(event.ID); err != nil {
-			return err
-		}
-	case "kill", "die", "stop", "destroy":
-		log().Debugf("resetting stats: id=%s", event.ID)
-		if err := b.resetStats(event.ID); err != nil {
-=======
 		image := c.Config.Image
 
 		// TODO: match rules
@@ -121,15 +89,12 @@ func (b *Beacon) HandleEvent(event *dockerclient.Event) error {
 		image := c.Config.Image
 		log().Debugf("resetting stats: id=%s image=%s", event.ID, image)
 		if err := b.resetStats(event.ID, image); err != nil {
->>>>>>> af48d45... beacon extension
 			return err
 		}
 	}
 
 	return nil
 }
-<<<<<<< HEAD
-=======
 
 func (b *Beacon) ruleMatch(cfg *dockerclient.ContainerConfig) bool {
 	// TODO
@@ -149,4 +114,3 @@ func (b *Beacon) startStats(id string, image string) error {
 func (b *Beacon) handleStats(id string, cb dockerclient.StatCallback, ec chan error, args ...interface{}) {
 	go b.client.StartMonitorStats(id, cb, ec, args...)
 }
->>>>>>> af48d45... beacon extension
