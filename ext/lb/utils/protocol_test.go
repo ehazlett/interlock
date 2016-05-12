7699dfac693cf0302473c3a17f38d5452eb2b397
Stéphane Cottin <stephane.cottin@vixns.com>
2016-05-13 00:16:12 +0200

    fix protocol test

--- ext/lb/utils/protocol_test.go
+++ ext/lb/utils/protocol_test.go
@@ -7,26 +7,10 @@ import (
 	"github.com/samalba/dockerclient"
 )
 
-func TestProtocol(t *testing.T) {
-	testProtocol := "proxy"
-
-	cfg := &dockerclient.ContainerConfig{
-		Protocol: testProtocol,
-	}
-
-	protocol := Protocol(cfg)
-
-	if protocol != testProtocol {
-		t.Fatalf("expected %s; received %s", testProtocol, protocol)
-	}
-}
-
 func TestProtocolLabel(t *testing.T) {
-	testProtocol := "foo"
 	testLabelProtocol := "proxy"
 
 	cfg := &dockerclient.ContainerConfig{
-		Protocol: testProtocol,
 		Labels: map[string]string{
 			ext.InterlockProtocolLabel: testLabelProtocol,
 		},
