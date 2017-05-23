cc681be05d8e342a59713fed3762afa4bada4b39
Evan Hazlett <ejhazlett@gmail.com>
2017-05-23 12:13:34 -0400

    add upstream_addr to nginx log format
    Signed-off-by: Evan Hazlett <ejhazlett@gmail.com>

--- ext/lb/nginx/template.go
+++ ext/lb/nginx/template.go
@@ -20,7 +20,7 @@ http {
     server_names_hash_bucket_size 128;
     client_max_body_size 2048M;
 
-    log_format  main  '$remote_addr - $remote_user [$time_local] "$request" '
+    log_format  main  '$remote_addr - $remote_user [$upstream_addr] [$time_local] "$request" '
                       '$status $body_bytes_sent "$http_referer" '
                       '"$http_user_agent" "$http_x_forwarded_for"';
 
